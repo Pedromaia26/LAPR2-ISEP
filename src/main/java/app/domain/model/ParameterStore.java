ca9bd0f87a5726378084b02857291516786f381f
1201000 <1201000@isep.ipp.pt>
2021-05-07 09:56:36 +0100

    US10 starting code

---
+++ src/main/java/app/domain/model/ParameterStore.java
@@ -0,0 +1,25 @@
+package app.domain.model;
+
+import java.util.List;
+
+public class ParameterStore {
+
+    private List<Parameter> parameterList;
+
+    public Parameter createParameter(String code, String shortName, String description){
+        return new Parameter(code, shortName, description);
+    }
+
+    public boolean validateParameter(Parameter parameter){
+        if (parameter == null){
+            return false;
+        }
+        return ! this.parameterList.contains(parameter);
+    }
+
+    public boolean saveParameter(Parameter parameter) {
+        if (!validateParameter(parameter))
+            return false;
+        return this.parameterList.add(parameter);
+    }
+}
