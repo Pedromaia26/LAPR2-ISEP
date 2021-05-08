2398ae0365af854df8468d714f4f60d0b5c9a59b
LAPTOP-042NG862\Tiago <1201276@isep.ipp.pt>
2021-05-08 23:33:06 +0100

    ParameterCategoryStoreTest validateParameterCategory getter and setter tests

--- src/test/java/app/domain/model/ParameterCategoryTest.java
+++ src/test/java/app/domain/model/ParameterCategoryTest.java
@@ -1,5 +1,6 @@
 package app.domain.model;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -21,4 +22,42 @@ public class ParameterCategoryTest {
     }
 
 
+    @Test
+    public void getCode() {
+        ParameterCategory pc = new ParameterCategory("Hemogram", "0a98h");
+        Company c = new Company("Many Labs");
+        String a = pc.getCode();
+
+        Assert.assertEquals("0a98h", a);
+    }
+
+    @Test
+    public void getName() {
+        ParameterCategory pc = new ParameterCategory("Hemogram", "0a98h");
+        Company c = new Company("Many Labs");
+        String name = pc.getName();
+
+        Assert.assertEquals("Hemogram", name);
+    }
+
+    @Test
+    public void setName() {
+        ParameterCategory pc = new ParameterCategory("Hemogram", "ap190");
+
+        pc.setName("Antibodies");
+        String name = pc.getName();
+
+        Assert.assertEquals ("Antibodies", name);
+    }
+
+    @Test
+    public void setCode() {
+        ParameterCategory pc = new ParameterCategory("Hemogram", "ap190");
+
+        pc.setCode("09kkl");
+        String code = pc.getCode();
+
+        Assert.assertEquals ("09kkl", code);
+
+    }
 }
