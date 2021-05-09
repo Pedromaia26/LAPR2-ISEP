06b63e4b9513e9416c41fd5bf9f8ef73c27f7c40
Pedro Fernandes <1201384@isep.ipp.pt>
2021-05-09 20:45:15 +0000

    RegisLaboratoryController.java Final

---
+++ src/main/java/app/controller/RegisLaboratoryController.java
@@ -0,0 +1,40 @@
+package app.controller;
+
+import app.domain.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegistLaboratoryController {
+    private Company company;
+    private TestType tt;
+    private TestTypeStore testTypeStore;
+    private LaboratoryStore LaboratoryStore;
+    private Laboratory cl;
+
+
+    public RegistLaboratoryController() {
+        this(App.getInstance().getCompany());
+    }
+
+    public RegistLaboratoryController(Company company) {
+        this.company = company;
+    }
+
+    public boolean createLaboratory(String laboratoryID, String name, String address, int phoneNumber, int tinNumber, List <TestType> TestTypes) {
+        List <TestType> tt = new ArrayList<>();
+        for (TestType tts: TestTypes){
+            tt.add(this.company.getTestTypeStore().getTestTypeByCode(tts.getCode()));
+        }
+        this.cl = this.company.getLaboratoryStore().createLaboratory(laboratoryID, name, address, phoneNumber, tinNumber, TestTypes);
+        return this.company.getLaboratoryStore().validateLaboratory(cl);
+    }
+
+    public TestTypeStore getTestTypeStore() {
+        return this.company.getTestTypeStore();
+    }
+
+    public boolean saveLaboratory () {
+        return this.company.getLaboratoryStore().saveLaboratory(cl);
+    }
+}
