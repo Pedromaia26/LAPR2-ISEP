8476013ef9d7dbee9bf0b77042e942ea38eb4e35
Pedro_Maia <1201381@isep.ipp.pt>
2021-05-09 23:15:48 +0100

    US7 class Employee test

---
+++ src/test/app/domain/model/EmployeeTest.java
@@ -0,0 +1,92 @@
+package app.domain.model;
+
+import auth.domain.model.Email;
+import auth.domain.model.UserRole;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeTest {
+
+    @Test
+    public void getUserRole() {
+        UserRole usrole = new UserRole("ADM", "adm");
+        Employee emp = new Employee(usrole, "P00001", "Pedro", "Porto", 912912912, new Email("pedro@gmail.com"), 1234);
+        Company c = new Company("Many Labs");
+        UserRole userRole = emp.getUserRole();
+
+        Assert.assertEquals(userRole, usrole);
+    }
+
+    @Test
+    public void getEmployeeId() {
+
+        UserRole usrole = new UserRole("ADM", "adm");
+        Employee emp = new Employee(usrole, "P00001", "Pedro", "Porto", 912912912, new Email("pedro@gmail.com"), 1234);
+        Company c = new Company("Many Labs");
+        String employeeId = emp.getEmployeeId();
+
+        Assert.assertEquals(employeeId, "P00001");
+    }
+
+    @Test
+    public void getName() {
+        UserRole usrole = new UserRole("ADM", "adm");
+        Employee emp = new Employee(usrole, "P00001", "Pedro", "Porto", 912912912, new Email("pedro@gmail.com"), 1234);
+        Company c = new Company("Many Labs");
+        String name = emp.getName();
+
+        Assert.assertEquals(name, "Pedro");
+    }
+
+    @Test
+    public void getAdress() {
+        UserRole usrole = new UserRole("ADM", "adm");
+        Employee emp = new Employee(usrole, "P00001", "Pedro", "Porto", 912912912, new Email("pedro@gmail.com"), 1234);
+        Company c = new Company("Many Labs");
+        String address = emp.getAdress();
+
+        Assert.assertEquals(address, "Porto");
+    }
+
+    @Test
+    public void getPhoneNumber() {
+        UserRole usrole = new UserRole("ADM", "adm");
+        Employee emp = new Employee(usrole, "P00001", "Pedro", "Porto", 912912912, new Email("pedro@gmail.com"), 1234);
+        Company c = new Company("Many Labs");
+        long phoneNumer = emp.getPhoneNumber();
+
+        Assert.assertEquals(phoneNumer, 912912912);
+    }
+
+    @Test
+    public void getEmail() {
+        UserRole usrole = new UserRole("ADM", "adm");
+        Email e = new Email("pedro@gmail.com");
+        Employee emp = new Employee(usrole, "P00001", "Pedro", "Porto", 912912912, e, 1234);
+        Company c = new Company("Many Labs");
+        Email email = emp.getEmail();
+
+        Assert.assertEquals(email, e);
+    }
+
+    @Test
+    public void getSocCode() {
+        UserRole usrole = new UserRole("ADM", "adm");
+        Employee emp = new Employee(usrole, "P00001", "Pedro", "Porto", 912912912, new Email("pedro@gmail.com"), 1234);
+        Company c = new Company("Many Labs");
+        int socCode = emp.getSocCode();
+
+        Assert.assertEquals(socCode, 1234);
+    }
+
+    @Test
+    public void testToString() {
+        UserRole usrole = new UserRole("ADM", "adm");
+        Employee emp = new Employee(usrole, "P00001", "Pedro", "Porto", 912912912, new Email("pedro@gmail.com"), 1234);
+        String a = emp.toString();
+
+        Assert.assertEquals("Employee{ userRole=ADM - adm, employeeId='P00001', name='Pedro', adress='Porto', phoneNumber=912912912, email=pedro@gmail.com, socCode=1234}", a);
+    }
+}
