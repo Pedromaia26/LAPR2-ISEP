8e07817d17cca59b96bad7af236e5dc8b3630f9a
1201000 <1201000@isep.ipp.pt>
2021-05-09 23:17:27 +0100

    ParameterCategoryTest mutation tests

--- src/test/java/app/domain/model/ParameterCategoryTest.java
+++ src/test/java/app/domain/model/ParameterCategoryTest.java
@@ -71,8 +71,13 @@ public class ParameterCategoryTest {
     public void testEquals() {
         ParameterCategory pc = new ParameterCategory("BloodC", "okjn7");
         ParameterCategory pc1 = new ParameterCategory("BloodC", "okjn7");
+        ParameterCategory pc2 = new ParameterCategory("Blood", "okjn7");
+        ParameterCategory pc3 = new ParameterCategory("BloodC", "11111");
+
 
         Assert.assertEquals(pc, pc1);
+        Assert.assertNotEquals(pc,pc2);
+        Assert.assertNotEquals(pc1, pc3);
 
     }
 
@@ -95,4 +100,5 @@ public class ParameterCategoryTest {
 
         Assert.assertNotEquals(pc, pc1);
     }
+
 }
