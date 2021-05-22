7ea0935608afac291f9d63ca9f45035f54d5f0a3
Pedro_Maia <1201381@isep.ipp.pt>
2021-05-22 16:34:31 +0100

    US4 - Test constructor and get methods

--- src/main/java/app/domain/model/Test.java
+++ src/main/java/app/domain/model/Test.java
@@ -1,34 +1,51 @@
 package app.domain.model;
 
-import app.controller.App;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Test {
 
-    /**
-     *
-     */
+    private String code;
+    private long nhsCode;
     private LabOrder labOrder;
-
-    private List<TestParameter> testParameterList;
 
     /**
      * List containing the samples.
      */
     private List<Sample> sample = new ArrayList<>();
 
-    public Test(){
+    public Test(String code, long nhsCode, LabOrder labOrder){
+
+        if (code.trim().length() != 10)
+            throw new IllegalArgumentException("Code should have 10 digits");
+        this.code = code;
+
+        if (String.valueOf(nhsCode).length() != 10)
+            throw new IllegalArgumentException("National Health Service Code should have 10 digits");
+
+        this.nhsCode = nhsCode;
+
+        this.labOrder = labOrder;
 
     }
 
     /**
-     * Constructs an instance of Test, receiving by parameter a lab order.
-     * @param labOrder The lab order relating to the test.
+     * Returns the code of a test.
+     * @return the code of a test .
      */
-    public Test(LabOrder labOrder) {
-        this.labOrder = labOrder;
+
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * Returns the National Health Service code of a test.
+     * @return the National Health Service code of a test.
+     */
+
+    public long getNhsCode() {
+        return nhsCode;
     }
 
 
@@ -50,32 +67,36 @@ public class Test {
         return sample;
     }
 
-
-
     /**
      * Returns the test parameter from a test.
      * @param parameterCode receives a parameter code by parameter and proceeds to check if this code exists.
+     * @return the test parameter intended if the code exists. If not, informs the user that the code does not exist.
      */
 
-    public void addTestResult(String parameterCode, String result, String metric){
-        TestParameter tParam =  getTestParameterFor(parameterCode);
-
-    }
-
-    private TestParameter getTestParameterFor(String parameterCode){
-        for (TestParameter tParam : testParameterList) {
-            if (tParam.getParameter().getCode().equals(parameterCode)) {
-                return tParam;
-            }
+    public Parameter getTestParameterFor(String parameterCode){
+        for (Parameter testParam: labOrder.getParameters()) {
+            if (parameterCode.equals(testParam.getCode()))
+                return testParam;
         }
-        throw new IllegalArgumentException("There is no parameter with such code!");
+        throw new IllegalArgumentException ("There is no parameter with such code");
     }
 
     /**
      * Returns the textual description of a test.
      * @return characteristics of a test.
      */
+
+
     @Override
+
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Test test = (Test) o;
+        return Objects.equals(code, test.code) && Objects.equals(nhsCode, test.nhsCode) && Objects.equals(labOrder, test.labOrder);
+    }
+
     public String toString() {
         return "Test:" + labOrder + ", sample=" + sample;
     }
