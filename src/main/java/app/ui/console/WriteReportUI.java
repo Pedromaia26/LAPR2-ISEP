0800dded3a3b3e749c1d1231cb89078036019167
Pedro_Maia <1201381@isep.ipp.pt>
2021-05-25 13:48:58 +0100

    US14 - adding ui and controller (missing in last commit)

---
+++ src/main/java/app/ui/console/WriteReportUI.java
@@ -0,0 +1,89 @@
+package app.ui.console;
+
+import app.controller.App;
+import app.controller.WriteReportController;
+import app.domain.model.ClientDTO;
+import app.domain.model.Test;
+import app.domain.model.TestDTO;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class WriteReportUI implements Runnable {
+    public WriteReportUI()
+    {
+
+    }
+    @Override
+    public void run() {
+        Scanner ler = new Scanner(System.in);
+        WriteReportController controller = new WriteReportController();
+
+        List<TestDTO> lTestsDto = new ArrayList<>();
+
+        int testop;
+        TestDTO testDto = null;
+        boolean exists = false;
+
+        lTestsDto = controller.getTests();
+
+        System.out.println("List of tests to be reported:");
+        for (TestDTO testdto: lTestsDto){
+            System.out.printf("%d - %s", lTestsDto.indexOf(testdto)+1, testdto);
+            System.out.println("---------");
+        }
+        System.out.println("Select one test of the list:");
+        testop = ler.nextInt();
+        if (testop <= lTestsDto.size() && testop >= 1){
+            testDto = lTestsDto.get(testop-1);
+            exists = true;
+        }
+        if (exists){
+            controller.getResultParameters(testDto);
+        }
+        /*System.out.print("Name: ");
+        String name = ler.nextLine();
+        System.out.print("Email: ");
+        String email = ler.nextLine();
+        System.out.print("Phone number: ");
+        long phoneNumber = ler.nextLong();
+        System.out.print("Citizen Card Number: ");
+        long ccn = ler.nextLong();
+        System.out.print("National Health Service number: ");
+        long nhs = ler.nextLong();
+        System.out.print("Tax Identification Number: ");
+        long tif = ler.nextLong();
+        System.out.print("Birth date: ");
+        ler.nextLine();
+        String birth = ler.nextLine();
+
+        System.out.print("Sex(if you dont want to write it, leave it blank): ");
+        String sex = ler.nextLine();
+
+
+        if(controller.createNewClient(new ClientDTO(ccn,nhs,birth,sex,tif,email,name,phoneNumber))){
+            System.out.println("--------------------------");
+            System.out.println("Please confirm the data:");
+            System.out.println(String.format("CCN: %d\nNHS: %d\nBirth date: %s\nSex: %s\nTIN: %d\nEmail: %s\nName: %s\nPhone Number: %s", ccn, nhs, birth,sex,tif,email,name,phoneNumber));
+            System.out.println("--------------------------");
+            System.out.println(" 1 --> Confirm");
+            System.out.println(" 2 --> Cancel");
+            int confirm = ler.nextInt();
+            if(confirm == 1){
+                try {
+                    if(controller.saveClient()){
+                        System.out.println("Client registered successfully.");
+                    }else{
+                        System.out.println("Client register error.");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }*/
+
+    }
+}
