f3fb6c689976780467becc1c57b01c9271bb1865
Eduardo Moreira da Silva <1201371@isep.ipp.pt>
2021-05-27 12:07:53 +0100

    Redo the Test class tests and others. only one assert by test

--- src/test/java/app/domain/model/SampleTest.java
+++ src/test/java/app/domain/model/SampleTest.java
@@ -185,30 +185,184 @@ public class SampleTest {
         nteste.addSample(s);
         Sample s1 = new Sample(c);
         nteste.addSample(s1);
-        Sample s2 =null;
-
-        SampleDTO s3= new SampleDTO("asda");
-
-        Sample s4 = new Sample(c2);
-
-        nteste.addSample(s4);
-
-
 
 
 
         Assert.assertNotEquals(s,s1);
 
+
+
+    }
+
+    @Test
+    public void testEqualsNull() {
+        Company c= new Company("ManyLabs");
+        Company c2= new Company("ManyLabs2");
+
+        ParameterCategory pc = new ParameterCategory("hemogram", "09090");
+
+        Parameter p = new Parameter("01981", "aa", "blood", pc);
+        List<Parameter> param = new ArrayList<>();
+
+        param.add(p);
+
+        ParameterCategory pc1 = new ParameterCategory("Immunity", "11111");
+        ParameterCategory pc2 = new ParameterCategory("Hemogram", "10019");
+        c.getParameterCategoryStore().addToList(pc1);
+        c.getParameterCategoryStore().addToList(pc2);
+
+        List<ParameterCategory> listPC = new ArrayList<>();
+        ParameterCategory pca = c.getParameterCategoryStore().getParameterCategoryByCode("10019");
+
+        listPC.add(pca);
+        TestType testesss = new TestType("asd","asd","12345",listPC);
+
+        c.getTestTypeStore().addToList(testesss);
+
+        LabOrder labOrder= new LabOrder(testesss,param);
+
+        c.getLabOrderStore().addToList(labOrder);
+
+        app.domain.model.Test nteste=new app.domain.model.Test("1234567890",1234123412L,labOrder);
+
+
+        c.getTestStore().addToList(nteste);
+
+
+        Sample s = new Sample(c);
+
+        Sample s2 =null;
+
         assertNotEquals(s,s2);
+
+    }
+
+    @Test
+    public void testEqualsDifferentClasses() {
+        Company c= new Company("ManyLabs");
+        Company c2= new Company("ManyLabs2");
+
+        ParameterCategory pc = new ParameterCategory("hemogram", "09090");
+
+        Parameter p = new Parameter("01981", "aa", "blood", pc);
+        List<Parameter> param = new ArrayList<>();
+
+        param.add(p);
+
+        ParameterCategory pc1 = new ParameterCategory("Immunity", "11111");
+        ParameterCategory pc2 = new ParameterCategory("Hemogram", "10019");
+        c.getParameterCategoryStore().addToList(pc1);
+        c.getParameterCategoryStore().addToList(pc2);
+
+        List<ParameterCategory> listPC = new ArrayList<>();
+        ParameterCategory pca = c.getParameterCategoryStore().getParameterCategoryByCode("10019");
+
+        listPC.add(pca);
+        TestType testesss = new TestType("asd","asd","12345",listPC);
+
+        c.getTestTypeStore().addToList(testesss);
+
+        LabOrder labOrder= new LabOrder(testesss,param);
+
+        c.getLabOrderStore().addToList(labOrder);
+
+        app.domain.model.Test nteste=new app.domain.model.Test("1234567890",1234123412L,labOrder);
+
+
+        c.getTestStore().addToList(nteste);
+
+
+        Sample s = new Sample(c);
+
+        SampleDTO s3= new SampleDTO("asda");
 
         assertNotEquals(s,s3);
 
+    }
+
+    @Test
+    public void testEqualsAlreadyAdded() {
+        Company c= new Company("ManyLabs");
+        Company c2= new Company("ManyLabs2");
+
+        ParameterCategory pc = new ParameterCategory("hemogram", "09090");
+
+        Parameter p = new Parameter("01981", "aa", "blood", pc);
+        List<Parameter> param = new ArrayList<>();
+
+        param.add(p);
+
+        ParameterCategory pc1 = new ParameterCategory("Immunity", "11111");
+        ParameterCategory pc2 = new ParameterCategory("Hemogram", "10019");
+        c.getParameterCategoryStore().addToList(pc1);
+        c.getParameterCategoryStore().addToList(pc2);
+
+        List<ParameterCategory> listPC = new ArrayList<>();
+        ParameterCategory pca = c.getParameterCategoryStore().getParameterCategoryByCode("10019");
+
+        listPC.add(pca);
+        TestType testesss = new TestType("asd","asd","12345",listPC);
+
+        c.getTestTypeStore().addToList(testesss);
+
+        LabOrder labOrder= new LabOrder(testesss,param);
+
+        c.getLabOrderStore().addToList(labOrder);
+
+        app.domain.model.Test nteste=new app.domain.model.Test("1234567890",1234123412L,labOrder);
+
+
+        c.getTestStore().addToList(nteste);
+
+
+        Sample s = new Sample(c);
+
+        Sample s4 = new Sample(c2);
+
         assertEquals(s,s4);
 
+    }
+
+    @Test
+    public void testEqualsSameSample() {
+        Company c= new Company("ManyLabs");
+        Company c2= new Company("ManyLabs2");
+
+        ParameterCategory pc = new ParameterCategory("hemogram", "09090");
+
+        Parameter p = new Parameter("01981", "aa", "blood", pc);
+        List<Parameter> param = new ArrayList<>();
+
+        param.add(p);
+
+        ParameterCategory pc1 = new ParameterCategory("Immunity", "11111");
+        ParameterCategory pc2 = new ParameterCategory("Hemogram", "10019");
+        c.getParameterCategoryStore().addToList(pc1);
+        c.getParameterCategoryStore().addToList(pc2);
+
+        List<ParameterCategory> listPC = new ArrayList<>();
+        ParameterCategory pca = c.getParameterCategoryStore().getParameterCategoryByCode("10019");
+
+        listPC.add(pca);
+        TestType testesss = new TestType("asd","asd","12345",listPC);
+
+        c.getTestTypeStore().addToList(testesss);
+
+        LabOrder labOrder= new LabOrder(testesss,param);
+
+        c.getLabOrderStore().addToList(labOrder);
+
+        app.domain.model.Test nteste=new app.domain.model.Test("1234567890",1234123412L,labOrder);
+
+
+        c.getTestStore().addToList(nteste);
+
+
+        Sample s = new Sample(c);
+
+
+
         assertEquals(s,s);
-
-
-
 
     }
 }
