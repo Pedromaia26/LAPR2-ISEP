8f0dc3d0f73ad7a1159ffbb9ef7c5b4a31549075
1201000 <1201000@isep.ipp.pt>
2021-05-27 16:22:01 +0100

    Create Laboratory Coordinator UI

---
+++ src/main/java/app/ui/console/LaboratoryCoordinatorUI.java
@@ -0,0 +1,28 @@
+package app.ui.console;
+
+import app.ui.console.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LaboratoryCoordinatorUI implements Runnable{
+    public LaboratoryCoordinatorUI()
+    {
+    }
+
+    public void run(){
+        List<MenuItem> options = new ArrayList<>();
+        options.add(new MenuItem("Validate the work done", new ValidateWorkDoneUI()));
+        int option;
+        do
+        {
+            option = Utils.showAndSelectIndex(options, "\n\nReceptionist Menu:");
+
+            if ( (option >= 0) && (option < options.size()))
+            {
+                options.get(option).run();
+            }
+        }
+        while (option != -1 );
+    }
+}
---
+++ src/main/java/app/ui/console/ValidateWorkDoneUI.java
@@ -0,0 +1,12 @@
+package app.ui.console;
+
+import java.util.Scanner;
+
+public class ValidateWorkDoneUI implements Runnable {
+    public ValidateWorkDoneUI(){
+    }
+
+
+    public void run() {
+    }
+}
