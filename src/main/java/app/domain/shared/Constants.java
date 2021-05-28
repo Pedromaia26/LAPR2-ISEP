e61f015f4d2abeb20b2e30f2e027b8d0055f7a3e
LAPTOP-042NG862\Tiago <1201276@isep.ipp.pt>
2021-05-28 10:09:18 +0100

    TestType update

--- src/main/java/app/domain/shared/Constants.java
+++ src/main/java/app/domain/shared/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String ROLE_ADMIN = "ADMINISTRATOR";
     public static final String ROLE_REC = "RECEPTIONIST";
     public static final String ROLE_MLT = "MEDICAL LAB TECHNICIAN";
-    public static final String ROLE_CCT = "CLINICAL CHEMESTRY TECHNOLOGIST";
+    public static final String ROLE_CCT = "CLINICAL CHEMISTRY TECHNOLOGIST";
     public static final String ROLE_LC = "LABORATORY COORDINATOR";
     public static final String ROLE_SD = "SPECIALIST DOCTOR";
     public static final String ROLE_C = "CLIENT";
