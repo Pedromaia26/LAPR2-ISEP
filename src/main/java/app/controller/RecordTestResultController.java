8b416a9e9f2becaa474579a6d441f52528bc23bd
LAPTOP-042NG862\Tiago <1201276@isep.ipp.pt>
2021-05-30 10:43:20 +0100

    Adding some changes in RecordTestResultController.

--- src/main/java/app/controller/RecordTestResultController.java
+++ src/main/java/app/controller/RecordTestResultController.java
@@ -12,21 +12,31 @@ public class RecordTestResultController {
     private List<Test> listT = new ArrayList<>();
     private List<TestDTO> listTDto = new ArrayList<>();
     private TestMapper tMapper;
+    private TestParameterMapper tpMapper;
     private List <TestParameter> testParameterList = new ArrayList<>();
     private List <TestParameterDto> testParameterDto = new ArrayList<>();
     private Test test;
     private Company c;
 
+
     public RecordTestResultController(){
         this.c = App.getInstance().getCompany();
         tStore = App.getInstance().getCompany().getTestStore();
         tMapper = new TestMapper();
+        tpMapper = new TestParameterMapper();
     }
 
     public Test getTestByBarcode(String code){
         this.test = tStore.getTestByBarcode(code);
         return tStore.getTestByBarcode(code);
 
+    }
+
+    public List <TestParameterDto> getTestParameters(String barcode){
+        test = tStore.getTestByBarcode(barcode);
+        testParameterList = test.getTestParameter();
+        testParameterDto = tpMapper.toDto(testParameterList);
+        return testParameterDto;
     }
 
     public List<TestDTO> getTestListStore(){
